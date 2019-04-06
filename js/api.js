8f7ec0e50d7eff5be67384b366bd34891448637a
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-06 13:55:22 -0500

    Send form data to server

--- js/api.js
+++ js/api.js
@@ -1,13 +1,22 @@
 const api = (function () {
+  function sendRequest (method, path, body) {
+    const req = new XMLHttpRequest();
+    req.open(method, path);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.send(JSON.stringify(body));
+  }
+
+  // sign up
   const signupButton = document.querySelector('#signupButton');
 
   if (signupButton) {
     signupButton.addEventListener('click', (e) => {
-      const req = new XMLHttpRequest();
-      const body = { test: 'body' };
-      req.open('POST', '/signup');
-      req.setRequestHeader('Content-Type', 'application/json');
-      req.send(JSON.stringify(body));
+      e.preventDefault();
+      const name = document.querySelector('#signupForm #name').value;
+      const address = document.querySelector('#signupForm #address').value;
+      const body = { name, address };
+      console.log(name, address);
+      sendRequest('POST', '/signup', body);
     });
   }
 })();
